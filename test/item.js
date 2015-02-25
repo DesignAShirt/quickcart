efdfa09433d19dbee9a8c28b9fb85635865752b8
Cory Mawhorter <cory.mawhorter@gmail.com>
2015-02-24 23:51:16 -0800

    tests

---
+++ test/item.js
@@ -0,0 +1,82 @@
+/**
+ * cart
+ *
+ *    Library test
+ */
+
+'use strict'
+
+var assert = require('assert')
+  , Item = require('../lib/cart').Item;
+
+describe('Item', function() {
+  describe('#ctor()', function(){
+    it('should instantiate', function() {
+      var item = new Item();
+      assert.strictEqual(true, item instanceof Item);
+      assert.strictEqual(item.constructor, Item);
+    });
+    it('should auto id', function() {
+      var item = new Item();
+      assert.notEqual(item.id, false);
+    });
+    it('should accept obj as arg', function() {
+      var value = 'world'
+        , id = 1
+        , price = 2
+        , signature = 'three'
+        , item;
+      assert.doesNotThrow(function() {
+        item = new Item({
+          id: id,
+          price: price,
+          signature: signature,
+          properties: { hello: value },
+        });
+      });
+      assert.strictEqual(item.id, id);
+      assert.strictEqual(item.price, price);
+      assert.strictEqual(item.signature, signature);
+      assert.strictEqual(item.property('hello'), value);
+    });
+  });
+
+  describe('#property()', function(){
+    it('should allow getting', function() {
+      var value = value = 'hello'
+        , item = new Item({ properties: { hello: value } });
+      assert.strictEqual(item.property('hello'), value);
+    });
+    it('should allow setting', function() {
+      var value = 'hello'
+        , value2 = 'world'
+        , item = new Item({ properties: { hello: value } });
+      assert.strictEqual(item.property('hello'), value);
+      item.property('hello', value2);
+      assert.strictEqual(item.property('hello'), value2);
+    });
+    it('should support chaining multiple sets', function() {
+      var value = 'hello'
+        , value2 = 'world'
+        , item = new Item();
+      item
+        .property('name', value)
+        .property('description', value2);
+      assert.strictEqual(item.property('name'), value);
+      assert.strictEqual(item.property('description'), value2);
+    });
+    it('should trigger change events', function(done) {
+      var value = 'hello'
+        , value2 = 'world'
+        , item = new Item({ properties: { hello: value } });
+      item.on('change', function(property, v, prev) {
+        assert.strictEqual('hello', property);
+        assert.strictEqual(v, value2);
+        assert.strictEqual(prev, value);
+        done();
+      });
+      assert.strictEqual(item.property('hello'), value);
+      item.property('hello', value2);
+    });
+  });
+});
--- test/utils.js
+++ test/utils.js
@@ -6,56 +6,42 @@
 
 'use strict'
 
-var assert = require('assert'),
-Cart        = require('../lib/cart');
+var assert = require('assert')
+  , Cart = require('../lib/cart');
 
-describe('Cart.utils.toArray()', function() {
-  it('should take arguments and return an array', function() {
-    var result = Cart.utils.toArray(arguments);
-    assert.strictEqual(toString.call(result), '[object Array]');
-  });
+describe('Cart.utils', function() {
+  describe('#toArray()', function() {
+    it('should take arguments and return an array', function() {
+      var result = Cart.utils.toArray(arguments);
+      assert.strictEqual(toString.call(result), '[object Array]');
+    });
 
-  it('should accept an array', function() {
-    var arr = [];
-    var result = Cart.utils.toArray(arr);
-    assert.strictEqual(toString.call(result), '[object Array]');
-  });
+    it('should accept an array', function() {
+      var arr = [];
+      var result = Cart.utils.toArray(arr);
+      assert.strictEqual(toString.call(result), '[object Array]');
+    });
 
-  it('should wrap non array-like objs', function() {
-    assert.strictEqual(toString.call(Cart.utils.toArray('string')), '[object Array]');
-    assert.strictEqual(toString.call(Cart.utils.toArray(Infinity)), '[object Array]');
-    assert.strictEqual(toString.call(Cart.utils.toArray(-Infinity)), '[object Array]');
-    assert.strictEqual(toString.call(Cart.utils.toArray(Number.MAX_VALUE)), '[object Array]');
-    assert.strictEqual(toString.call(Cart.utils.toArray(Number.MIN_VALUE)), '[object Array]');
-    assert.strictEqual(toString.call(Cart.utils.toArray(1.1)), '[object Array]');
-    assert.strictEqual(toString.call(Cart.utils.toArray(true)), '[object Array]');
-    assert.strictEqual(toString.call(Cart.utils.toArray(false)), '[object Array]');
-    assert.strictEqual(toString.call(Cart.utils.toArray(null)), '[object Array]');
-    assert.strictEqual(toString.call(Cart.utils.toArray(void 0)), '[object Array]');
-  });
+    it('should wrap non array-like objs', function() {
+      assert.strictEqual(toString.call(Cart.utils.toArray('string')), '[object Array]');
+      assert.strictEqual(toString.call(Cart.utils.toArray(Infinity)), '[object Array]');
+      assert.strictEqual(toString.call(Cart.utils.toArray(-Infinity)), '[object Array]');
+      assert.strictEqual(toString.call(Cart.utils.toArray(Number.MAX_VALUE)), '[object Array]');
+      assert.strictEqual(toString.call(Cart.utils.toArray(Number.MIN_VALUE)), '[object Array]');
+      assert.strictEqual(toString.call(Cart.utils.toArray(1.1)), '[object Array]');
+      assert.strictEqual(toString.call(Cart.utils.toArray(true)), '[object Array]');
+      assert.strictEqual(toString.call(Cart.utils.toArray(false)), '[object Array]');
+      assert.strictEqual(toString.call(Cart.utils.toArray(null)), '[object Array]');
+      assert.strictEqual(toString.call(Cart.utils.toArray(void 0)), '[object Array]');
+    });
 
-  it('should never return reference to the input', function() {
-    var arr = []
-      , obj = {};
-    assert.notStrictEqual(Cart.utils.toArray(arr), arr);
-    assert.notStrictEqual(Cart.utils.toArray(obj), obj);
-    assert.notStrictEqual(Cart.utils.toArray(arguments), arguments);
-  });
-});
-
-describe('Cart.utils.applyProperties()', function() {
-  it('should extend an object', function() {
-    var obj1 = { a: null }
-      , obj2 = { a: 1 };
-    Cart.utils.applyProperties(obj1, obj2, new Error('Invalid Cart property passed'));
-    assert.strictEqual(obj1.a, obj2.a);
-  });
-
-  it('should throw if invalid key exists', function() {
-    var obj1 = { a: null }
-      , obj2 = { a: 1, errkey: null };
-    assert.throws(function() {
-      Cart.utils.applyProperties(obj1, obj2, new Error('Invalid Cart property passed'));
-    }, Error);
+    it('should never return reference to the input', function() {
+      var arr = []
+        , obj = {};
+      assert.notStrictEqual(Cart.utils.toArray(arr), arr);
+      assert.notStrictEqual(Cart.utils.toArray(obj), obj);
+      assert.notStrictEqual(Cart.utils.toArray(arguments), arguments);
+    });
   });
 });
+
